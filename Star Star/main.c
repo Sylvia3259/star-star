ee9b0f631a52048ad14436d5eca5c503a2726acc
unknown <newjincar33@gmail.com>
2021-03-13 23:34:09 +0900

    first commit

---
+++ Star Star/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	scanf_s("%d", &n);
+
+	n = 3*n/8;
+	for (int y = n; y >= -5*n/3; y--) {
+		for (int x = -2*n-1; x <= 2*n+1; x++)
+			printf((y <= x*2+n) + (y <= -x*2+n) + (y >= x/2-n) + (y >= -x/2-n) + (y <= 0) >= 4 ? "*" : " ");
+		puts("");
+	}
+
+	return 0;
+}
