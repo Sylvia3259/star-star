517bf97d26f5fc704d1f82db020523ff9dc72f82
unknown <newjincar33@gmail.com>
2021-03-14 01:15:35 +0900

    코드 정리

--- Star Star/main.c
+++ Star Star/main.c
@@ -7,7 +7,7 @@ int main(void) {
 	n = 3*n/8;
 	for (int y = n; y >= -5*n/3; y--) {
 		for (int x = -2*n-1; x <= 2*n+1; x++)
-			printf((y <= x*2+n) + (y <= -x*2+n) + (y >= x/2-n) + (y >= -x/2-n) + (y <= 0) >= 4 ? "*" : " ");
+			printf((y <= 2*x+n) + (y <= -2*x+n) + (y >= x/2-n) + (y >= -x/2-n) + (y <= 0) >= 4 ? "*" : " ");
 		puts("");
 	}
 
